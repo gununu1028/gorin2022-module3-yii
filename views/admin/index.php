b1a396c5907a43ad9e3d181c4333b3896426df0e
AtsushiMatsuda <amatsuda@zaico.co.jp>
2022-10-16 21:27:23 +0900

    シナリオ:A2d 既存の商品情報を削除する

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Url;
+use yii\bootstrap5\ActiveForm;
 
 $this->title = '管理画面';
 ?>
@@ -17,6 +18,7 @@ $this->title = '管理画面';
                 <th>ID</th>
                 <th>商品名</th>
                 <th></th>
+                <th></th>
             </tr>
             <?php for ($i = 0; $i < count($items); $i++) : ?>
                 <tr>
@@ -29,9 +31,26 @@ $this->title = '管理画面';
                     <td>
                         <a href="<?= Url::toRoute(['admin/edit-item/', 'id' => $items[$i]['id']]) ?>">編集</a>
                     </td>
+                    <td>
+                        <?php
+                        $form = ActiveForm::begin(
+                            ['action' => '/admin/delete-item']
+                        );
+                        ?>
+                        <input type="hidden" name="id" value="<?= $items[$i]['id'] ?>">
+                        <input type="submit" class="btn btn-link p-0 m-0" value="削除" onclick="clickDeleteButton(event)">
+                        <?php ActiveForm::end(); ?>
+                    </td>
                 </tr>
             <?php endfor; ?>
         </table>
         <a href="<?= Url::toRoute('admin/new-item') ?>">商品情報の新規登録</a>
-    </div>
-<?php endif; ?>
+    </div><?php endif; ?>
+<script>
+    function clickDeleteButton(event) {
+        event.preventDefault();
+        if (confirm('削除してよろしいですか？')) {
+            event.target.parentElement.submit();
+        }
+    }
+</script>
