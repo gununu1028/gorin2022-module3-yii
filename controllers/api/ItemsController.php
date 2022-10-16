477828c9d7bf428c1fcc72a893f672cdea7124f9
AtsushiMatsuda <amatsuda@zaico.co.jp>
2022-10-16 20:48:03 +0900

    RESTful API 商品情報の絞り込み

--- controllers/api/ItemsController.php
+++ controllers/api/ItemsController.php
@@ -8,15 +8,36 @@ use yii\web\Controller;
 use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
+use yii\db\Query;
 
 class ItemsController extends Controller
 {
     public function actionIndex()
     {
         Yii::$app->response->format = Response::FORMAT_JSON;
+        $r = Yii::$app->request;
+        $query = new Query;
+        $query->select('*')->from('items');
+        $where = [];
 
-        $items = Yii::$app->db->createCommand('SELECT * FROM items')->queryAll();
+        $shop_id = $r->get('shop_id');
+        if ($shop_id) {
+            $where['user_id'] = $shop_id;
+        }
 
+        $price = $r->get('price');
+        if ($price) {
+            $where['price'] = $price;
+        }
+
+        $query->where($where);
+
+        $title = $r->get('title');
+        if ($title) {
+            $query->andWhere(['like', 'name', $title]);
+        }
+
+        $items = $query->all();
         return $items;
     }
 }
