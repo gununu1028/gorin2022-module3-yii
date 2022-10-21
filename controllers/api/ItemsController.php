063612348510f7fbea1f5caba3af2964644d92a7
AtsushiMatsuda <amatsuda@zaico.co.jp>
2022-10-21 22:29:29 +0900

    不要な記述を削除

--- controllers/api/ItemsController.php
+++ controllers/api/ItemsController.php
@@ -3,11 +3,8 @@
 namespace app\controllers\api;
 
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
 use yii\db\Query;
 
 class ItemsController extends Controller
